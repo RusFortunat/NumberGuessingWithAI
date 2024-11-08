68ea3f0c078260d0e656349ed712be8f27288549
RusFortunat <ruslan.mukhamadiarov@gmail.com>
2024-11-08 11:06:22 +0100

    create SpringBoot project template with Spring Initializr

---
+++ src/main/java/com/guessNumbersWithAI/GuessNumbersWithAiApplication.java
@@ -0,0 +1,13 @@
+package com.guessNumbersWithAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuessNumbersWithAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuessNumbersWithAiApplication.class, args);
+	}
+
+}
