83add5db2d2376d61841bafaa756f2f1dd451420
RusFortunat <ruslan.mukhamadiarov@gmail.com>
2024-11-09 19:05:20 +0100

    got canvas working and sending image data to server

--- src/main/java/com/guessNumbersWithAI/model/Network.java
+++ src/main/java/com/guessNumbersWithAI/model/Network.java
@@ -3,44 +3,52 @@ package com.guessNumbersWithAI.model;
 // I will provide two options to the user: to load parameters of NN that I've trained myself in my other
 // project, or to load parameters that have been obtained from state-of-art PyTorch training method
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Network {
 
+    public String chosenModel;
+    public String testField;
     private int inputSize;
     private int hiddenSize;
     private int outputSize;
+    private ArrayList<Double> rawInput; // will be provided by user
     private double[] inputVector; // will be provided by user
     private double[] hiddenVector; // null at the beginning
     private double[] outputVector;
-    // the neural network parameters that will be loaded from the database
+    // the neural network parameters that will be loaded from the file
     private double[][] firstLayerWeights;
     private double[] firstLayerBiases;
     private double[][] secondLayerWeights;
     private double[] secondLayerBiases;
 
     // I will let the user load the parameters from the MySQL DB
-    public Network(int inputSize, int hiddenSize, int outputSize,
-                   double[][] firstLayerWeights, double[] firstLayerBiases,
-                   double[][] secondLayerWeights, double[] secondLayerBiases) {
-        this.inputSize = inputSize;
-        this.hiddenSize = hiddenSize;
-        this.outputSize = outputSize;
-        this.inputVector = new double[inputSize];
+    public Network() {
+        this.testField = "";
+        this.chosenModel = "";
+        this.inputSize = 28*28;
+        this.hiddenSize = 128;
+        this.outputSize = 10;
+        this.rawInput = new ArrayList<>(); // input that takes all three RGB channels
+        this.inputVector = new double[inputSize]; // we will convert raw input to grayscale image
         this.hiddenVector = new double[hiddenSize];
         this.outputVector = new double[outputSize];
-        this.firstLayerWeights = firstLayerWeights;
-        this.firstLayerBiases = firstLayerBiases;
-        this.secondLayerWeights = secondLayerWeights;
-        this.secondLayerBiases = secondLayerBiases;
+    }
+
+    // loads the parameters of the chosen network
+    public void loadNetworkParameters(){
+
     }
 
     // Forward propagation: the neural network gets user image input, that is converted to the input vector
     // and returns as an output the probabilities for different numbers
     public double[] forward(double[] input){
-        // forwardprop is very simple really; just do the following:
-        // 1. Compute [z] = [firstLayerWeights][input] + [firstLayerBiases];
-        // 2. Obtain the activation values of the hidden vector [y] by applying to [z] some activation function f([z]).
-        //    Here we use ReLU activation: f(z) > 0 ? z : 0;
-        // 3. Repeat for the next layer with secondLayerWeights and secondLayerBiases to get the [output] vector.
+        // forward propagation is not that hard, just do the following:
+        // 1. Take matrix product and add biases [z] = [firstLayerWeights]*[input] + [firstLayerBiases];
+        // 2. Obtain hidden activation values [y] by applying to [z] some activation function f([z]), i.e., mapping.
+        //    Here we use ReLU activation / mapping: f(z) > 0 ? z : 0;
+        // 3. Repeat 1.-2. for the next layer with secondLayerWeights and secondLayerBiases to get the [output] vector.
 
         // compute hidden activation values
         for(int i = 0; i < hiddenSize; i++){
@@ -71,4 +79,86 @@ public class Network {
         return outputVector;
     }
 
+
+    // getters
+
+    public String getChosenModel() {
+        return chosenModel;
+    }
+
+    public int getInputSize() {
+        return inputSize;
+    }
+
+    public int getHiddenSize() {
+        return hiddenSize;
+    }
+
+    public int getOutputSize() {
+        return outputSize;
+    }
+
+    public ArrayList<Double> getRawInput() {
+        return rawInput;
+    }
+
+    public double[] getInputVector() {
+        return inputVector;
+    }
+
+    public double[] getHiddenVector() {
+        return hiddenVector;
+    }
+
+    public double[] getOutputVector() {
+        return outputVector;
+    }
+
+    public double[][] getFirstLayerWeights() {
+        return firstLayerWeights;
+    }
+
+    public double[] getFirstLayerBiases() {
+        return firstLayerBiases;
+    }
+
+    public double[][] getSecondLayerWeights() {
+        return secondLayerWeights;
+    }
+
+    public double[] getSecondLayerBiases() {
+        return secondLayerBiases;
+    }
+
+
+    // setters
+
+
+    public void setChosenModel(String chosenModel) {
+        this.chosenModel = chosenModel;
+    }
+
+    public void setInputSize(int inputSize) {
+        this.inputSize = inputSize;
+    }
+
+    public void setHiddenSize(int hiddenSize) {
+        this.hiddenSize = hiddenSize;
+    }
+
+    public void setOutputSize(int outputSize) {
+        this.outputSize = outputSize;
+    }
+
+    public void setRawInput(ArrayList<Double> rawInput) {
+        this.rawInput = rawInput;
+    }
+
+    public String getTestField() {
+        return testField;
+    }
+
+    public void setTestField(String testField) {
+        this.testField = testField;
+    }
 }
